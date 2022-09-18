0c0235dac3c00d4f938f39cce5c466f93f07cdbf
Dragon of Shuu <68718280+MushroomEnder@users.noreply.github.com>
2022-09-18 13:05:41 -0600

    Image Start

--- build_tasks/commands.cmd
+++
@@ -1,4 +0,0 @@
-pip3.10 install pyinstaller
-pyinstaller math_is_easy.py -y --add-data=packages.json;. --add-data=utils/save_data/save_file.json;utils/save_data --hidden-import="pyperclip"
-cls
-.\dist\math_is_easy\math_is_easy.exe
